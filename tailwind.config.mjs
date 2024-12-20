cb5bb1ad2b1ddffd96a4e2832bccd3c067ce86b5
SergeiKrk <krjukov_s@mail.ru>
2024-12-20 21:10:37 +0400

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      fontFamily: {
+        comfortaa: ["Comfortaa", "sans-serif"],
+      },
+      fontWeight: {
+        regular: 400,
+        medium: 500,
+      },
+      colors: {
+        green: "var(--green)",
+        black: "var(--black)",
+        dark: "var(--dark)",
+        white: "var(--white)",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("preline/plugin")],
+};
